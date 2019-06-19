eb371f67a797061326e27012c86da9c27afa4222
KAI_Monmoy <aftarulislam@gmail.com>
2019-06-20 03:54:31 +0600

    :sparkles: Created Cat & Updated Home

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/cat.dart';
+
 class Home extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animation"),
+      ),
+      body: buildAnimation(),
+    );
+  }
+
+  Widget buildAnimation() {
+    return Cat();
   }
 }
