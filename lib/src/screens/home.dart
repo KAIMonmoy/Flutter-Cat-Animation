ea70a250c4f7afa204284914ae6c14bc2e6915d9
KAI_Monmoy <aftarulislam@gmail.com>
2019-06-20 11:09:47 +0600

    :bug: Fixing Animation Positioning

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -16,11 +16,11 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
     super.initState();
 
     catController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 200),
       vsync: this,
     );
 
-    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+    catAnimation = Tween(begin: -35.0, end: -80.0).animate(
       CurvedAnimation(
         parent: catController,
         curve: Curves.easeIn,
@@ -43,11 +43,14 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
         title: Text("Animation"),
       ),
       body: GestureDetector(
-        child: Stack(
-          children: <Widget>[
-            buildCatAnimation(),
-            buildBox(),
-          ],
+        child: Center(
+          child: Stack(
+            children: <Widget>[
+              buildCatAnimation(),
+              buildBox(),
+            ],
+            overflow: Overflow.visible,
+          ),
         ),
         onTap: onTap,
       ),
@@ -59,9 +62,11 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       animation: catAnimation,
       child: Cat(),
       builder: (BuildContext context, Widget child) {
-        return Container(
+        return Positioned(
           child: child,
-          margin: EdgeInsets.only(top: 100 - catAnimation.value),
+          top: catAnimation.value,
+          right: 0.0,
+          left: 0.0,
         );
       },
     );
